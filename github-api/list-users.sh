38858a6fbb686fdee3c49e78d803816124024840
Rithin129 <146566675+Rithin129@users.noreply.github.com>
2025-12-29 18:07:41 -0500

    Update list-users.sh

--- github-api/list-users.sh
+++ github-api/list-users.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#Author: Rithin 
+#About: list of user who have access to repo
+
 # GitHub API URL
 API_URL="https://api.github.com"
 
@@ -36,7 +39,15 @@ function list_users_with_read_access {
     fi
 }
 
+
 # Main script
 
 echo "Listing users with read access to ${REPO_OWNER}/${REPO_NAME}..."
+
 list_users_with_read_access
+function helper{
+expected_cmd_args=2
+if [ $# -ne $expected_cmd_args]; then
+echo "please execute script with required cmd args"
+echo "asd"
+}
